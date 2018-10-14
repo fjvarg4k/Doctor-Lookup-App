e042a5d89a68da9e5f909fcdae7fb3854bff094a
Francisco Vargas <fjvarg4934@gmail.com>
2018-10-14 09:13:33 -0500

    Attempted to convert zip code to lat + lng

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const doctorBaseUrl = 'https://api.betterdoctor.com/2016-03-01/doctors';
-const mapsBaseUrl = 'https://maps.googleapis.com/maps/api/js';
-const doctorAPIKey = // Insert your api key
-const mapsAPIKey = // Insert your api key
+const mapsBaseUrl = 'https://maps.googleapis.com/maps/api/geocode/json?';
+const doctorAPIKey = 'Insert your api key';
+const mapsAPIKey = 'Insert your api key';
 
-// function formatQueryParams(params) {
-//
-// }
-//
-// function displayResults(responseJson) {
-//
-// }
+function formatQueryParams(params) {
+
+}
+
+function displayResults(responseJson) {
+
+}
 
 
 // Using the provided user input and fetches the info from the BetterDoct API
 function getDoctorInfo(queryType, query, zipcode) {
-  getLocation(zipcode);
-  console.log(queryType);
+  let coords = getLocation(zipcode);
+  console.log(coords);
+  // let longitude;
+  // let latitude;
+  // coords.then(result => {
+  //   // console.log(result);
+  //   longitude = result.results[0].geometry.location.lat;
+  //   latitude = result.results[0].geometry.location.lng;
+  //   console.log(longitude, latitude);
+  // });
   // const params = {
   //   api_key: doctorAPIKey,
   //   name: queryType === 'doctor' ? query : '',
@@ -27,24 +35,47 @@ function getDoctorInfo(queryType, query, zipcode) {
 
 // Takes the user zipcode, converts it to latitude and longitude
 function getLocation(userZip) {
-  const url = `${mapsBaseUrl}?key=${mapsAPIKey}&address=${userZip}`;
+  let longitude;
+  let latitude;
+  // let coords;
+  const url = `${mapsBaseUrl}key=${mapsAPIKey}&address=${userZip}`;
   console.log(url);
+
+  return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => {
+      // console.log(responseJson);
+      // latitude = responseJson.results[0].geometry.location.lat;
+      // longitude = responseJson.results[0].geometry.location.lng;
+      // console.log(latitude, longitude);
+      // return [latitude, longitude];
+      return responseJson;
+    })
+    .catch(err => {
+      $('#error-message').text(`Something went wrong: ${err.message}`)
+    });
+  // let latitude = results.geometry[0].location.lat;
+  // let longitude = results.geometry[0].location.lng;
+  // console.log(lat, long);
 }
 
-// function getMapInfo() {
-//
-// }
+function getMapInfo() {
+
+}
 
 
 // Grabs the provided user input
 function watchForm() {
   $('form').on('submit', event => {
     event.preventDefault();
-    console.log('watchForm is running');
     const searchType = $('#search-options').val();
     const searchTerm = $('#search-doctor-symptom-input').val();
     const searchZip = $('#search-zip-input').val();
-    console.log(searchZip);
     getDoctorInfo(searchType, searchTerm, searchZip);
   });
 }
